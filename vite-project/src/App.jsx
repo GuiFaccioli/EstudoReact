3e7ddf3716a295cde5eb599673aa3d3d4699e720
Guilherme Faccioli <guifacciolic@gmail.com>
2026-03-06 10:09:23 -0300

    meu primeiro estudo de react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <Helloworld />
+      <Hello />
+      <World />
+      <h1>Estude!</h1>
+    </div>
+  );
+}
+function Helloworld() {
+  return <h1>Hello World!</h1>;
+}
+function Hello() {
+  return <h1>Hello</h1>;
+}
+
+function World() {
+  return <h1>World</h1>;
+}
+export default App;
